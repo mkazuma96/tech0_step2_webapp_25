4fa160d668dedeea6361672762cd559879a4e714
mkazuma96 <k.matsumoto9296@gmail.com>
2025-04-20 15:03:05 +0900

    UIのコードを追加しました

--- webapp_presentation.py
+++ webapp_presentation.py
@@ -10,6 +10,9 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from dotenv import load_dotenv
 import pyrebase
+import base64
+from PIL import Image
+from pathlib import Path
 
 #.envを呼び出せるようにする
 load_dotenv()
@@ -41,6 +44,69 @@ auth = firebase.auth()
 
 api_key =os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
+
+#ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+#UI変更
+#フォント変更
+def apply_custom_font():
+    font_path = "static/1.otf"
+    with open(font_path, "rb") as font_file:
+        font_data = font_file.read()
+        encoded_font = base64.b64encode(font_data).decode()
+    
+    st.markdown(
+        f"""
+        <style>
+        @font-face {{
+            font-family: 'CustomFont';
+            src: url(data:font/otf;base64,{encoded_font});
+        }}
+        * {{
+            font-family: 'CustomFont', sans-serif !important;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+apply_custom_font()
+
+# 背景画像をBase64形式でエンコード
+def get_base64_encoded_image(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# CSSに背景画像を適用
+background_image_path = "background.png"  # ローカル画像のパス
+encoded_image = get_base64_encoded_image(background_image_path)
+
+screencast_bg_css = f"""
+<style>
+    [data-testid="stApp"] {{
+        background-image: url("data:image/jpeg;base64,{encoded_image}");
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+</style>
+"""
+st.markdown(screencast_bg_css, unsafe_allow_html=True)
+
+# メッセージ枠のスタイルを変更するCSSを追加
+custom_style = """
+<style>
+    div[data-testid="stAlertContainer"] {
+        background-color: #fad67d; /* 背景色（薄いオレンジに変更） */ 
+        padding: 10px; /* 内側の余白 */
+        border-radius: 5px; /* 角を丸める */
+        textColor: #090547; # 紺色
+    }
+</style>
+"""
+st.markdown(custom_style, unsafe_allow_html=True)
+
+
 
 #ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 #サイドバー　メニュー
